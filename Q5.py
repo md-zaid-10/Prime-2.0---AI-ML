77fe834fc289e32be3bbe44c996597836dbdb41c
md-zaid-10 <mdzaid.de.123@gmail.com>
2026-03-30 13:59:19 +0200

    done

---
+++ Q5.py
@@ -0,0 +1,8 @@
+x = 10 + 3 * 2 ** 2
+
+print(x)
+
+# operator precedence 
+    # ** > * > +
+        # Therefore 2**2=4, 10+3*4, 10+12, 22
+        #  Ans = 22
---
+++ Q7.py
@@ -0,0 +1,8 @@
+temp = float(input("Enter the temperature in Celsius: "))
+
+print("The temperature in Celcius is: ",temp)
+
+fahrenheit = (temp * (9/5)) + 32
+
+print("The value of Temperature on Fahrenheit is :", fahrenheit)
+
